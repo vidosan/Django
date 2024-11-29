bcdcb5ee320a76a95448746d0940f697172ee473
Sereda Andrey <vidok_138@bk.ru>
2024-11-29 09:39:59 +0300

    Django_281124

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
+
+from .models import Women
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'}
-        ]
+]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
@@ -15,16 +20,20 @@ data_db = [
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
 
-cats_db = [{'id': 1, 'name': 'Актрисы'},
-           {'id': 2, 'name': 'Певицы'},
-           {'id': 3, 'name': 'Спортсменки'},]
+cats_db = [
+    {'id': 1, 'name': 'Актрисы'},
+    {'id': 2, 'name': 'Певицы'},
+    {'id': 3, 'name': 'Спортсменки'},
+]
 
 
 def index(request):
+    posts = Women.published.all()
+
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': posts,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
@@ -34,8 +43,17 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
@@ -50,13 +68,13 @@ def login(request):
     return HttpResponse("Авторизация")
 
 
-def show_category(request,cat_id):
+def show_category(request, cat_id):
     data = {
-            'title': 'Отображение по категориям',
-            'menu': menu,
-            'posts': data_db,
-            'cat_selected': cat_id,
-        }
+        'title': 'Отображение по рубоикам',
+        'menu': menu,
+        'posts': data_db,
+        'cat_selected': cat_id,
+    }
     return render(request, 'women/index.html', context=data)
 
 
